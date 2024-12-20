f6d91f2f288a25f5ac1b4ed59873b30654a3edf4
Rowan Brown <97682340+rowanjb@users.noreply.github.com>
2024-12-20 11:32:13 +0100

    Update mooring_analyses.py comments for CR

--- mooring_analyses.py
+++ mooring_analyses.py
@@ -12,26 +12,32 @@ def open_mooring_ml_data():
     """Opens CTD data in the mixed layer from the Weddell Sea mooring.
     We're interested because it seems to show a convective plume."""
 
+    # open the .mat file and specifically retrieve the day ('jul'), temp ('T'), salinity ('S') and pressure ('P')
     with open('../filepaths/mooring_filepath') as f: dirpath = f.readlines()[0][:-1] # the [0] accesses the first line, and the [:-1] removes the newline tag
     filepath = dirpath + '/CTD/Mooring/BGC_SBE.mat'
     mat = spio.loadmat(filepath)['SBE'] # SBE refers to Sea Bird (the instumentation company)
     jul, T, S, P  = mat['jul'][0], mat['T'][0], mat['S'][0], mat['P'][0] # un-nesting the data
+    
+    # creating list of dates that we'll use to interpolate the data onto
+    # specifically, we'll take daily averages of the measurements (perhaps I should weight these...)
     new_day_coord = list(range(85,462)) # we're going to interpolate onto these days
     start_date = datetime(2020,12,31,0,0,0) # basically just assuming this is the start date
     new_datetime_coords = [start_date + timedelta(days=i) for i in new_day_coord] # for the ds
 
-    def daily_avg_mooring_data(inmat): # locally-define func for getting the average of a variable over one day
+    def daily_avg_mooring_data(inmat): 
+        # locally-defined func for getting the average of a variable over one day
         out = np.empty((len(new_day_coord),6)) # initializing a nan array
         out[:] = np.nan
         for i in range(len(jul)):
-            if len(inmat[i]) > 0:
+            if len(inmat[i]) > 0: # basically if the sensor wasn't broken and the data therefore filled with nans
                 date = pd.to_datetime([start_date + timedelta(days=i[0]) for i in jul[i]])
                 df = pd.DataFrame(data={'date': date, 'var': [i[0] for i in inmat[i]]})
-                df = df[(df['date'] > '2021-03-26') & (df['date'] < '2022-04-07')]
+                df = df[(df['date'] > '2021-03-26') & (df['date'] < '2022-04-07')] # masking start+end dates
                 new_df = df.groupby(by=df['date'].dt.date).mean()
                 out[:,i] = np.array(new_df['var'])
         return out
-    
+
+    # create the xarray dataset
     ds = xr.Dataset(
         data_vars=dict(
             T=(["day","depth"], daily_avg_mooring_data(T)),
@@ -47,6 +53,7 @@ def open_mooring_ml_data():
 
     return ds
 
+# plotting temperature
 def temp_hovm(ds):
     f, ax = plt.subplots(figsize=(3, 2))
     ds.T.sel(depth=[-50,-135,-220]).plot.contourf('day','depth',ax=ax,levels=20,cbar_kwargs={'label': 'Temperature ($\degree C$)'})
@@ -55,6 +62,7 @@ def temp_hovm(ds):
     ax.set_title('Temperature at Weddell Sea mooring')
     plt.savefig('Temperature.png',bbox_inches='tight',dpi=450)
 
+# plotting temperature
 def sal_hovm(ds):
     f, ax = plt.subplots(figsize=(3, 2))
     ds.S.sel(depth=[-50,-135,-220]).plot.contourf('day','depth',ax=ax,levels=10,cbar_kwargs={'label': 'Salinity ($PSU$)'})
