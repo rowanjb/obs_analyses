c5e12efc040a66677f1b87b385ad1024a471a47a
rowanjb <rjb641@mun.ca>
2025-01-17 15:50:57 +0100

    ITP and Marlin float figs

--- itp_analyses.py
+++ itp_analyses.py
@@ -1,48 +1,229 @@
 # Rowan Brown, 12.12.2024
 # Munich, Germany
 
+import os
 import xarray as xr 
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
+import matplotlib.path as mpath
+import cartopy.crs as ccrs
+import cartopy.feature as feature
+import matplotlib.ticker as mticker
+import matplotlib.colors as colors
+import numpy as np
 
 def open_dat_file(itp):
     """Opens level 3 pressure-bin-averaged data at 1-db vertical resolution.
-    All ITPs should have associated .dat files, so this function /should/ be quite easy to use.
-    ITP132 doesn't have salinities in the cleaned .mat file due to a bad sensor; hopefully what good data exist are here instead."""
+    All ITPs should have associated .dat files in the grddata directory, which holds the level-3 "corrected" data.
+    Note I found ITP132 doesn't have salinities in the cleaned .mat file, which is why I'm using the .dat files."""
 
     with open('../filepaths/itp_filepath') as f: dirpath = f.readlines()[0][:-1] # the [0] accesses the first line, and the [:-1] removes the newline tag
-    filepath = dirpath+str(itp)+'/itp'+str(itp)+'grddata/itp'+str(itp)+'grd0694.dat'             # filepath to the processed data mat file
-    locpd = pd.read_csv(filepath, sep='\s+', nrows=2, engine='python')
-    datapd = pd.read_csv(filepath, sep='\s+', skiprows=2, skipfooter=1, engine='python')
-    lon = locpd.iloc[0,2]
-    lat = locpd.iloc[0,3]
-    pressure = datapd['pressure(dbar)']
-    temperature = datapd['temperature(C)']
-    salinity = datapd['salinity']
-    year = datetime(datapd['%year'],12,31)
-    print('test')
-    quit()
-    day = datapd['day']
-    date = datetime(year.iloc[10],12,31) + timedelta(days=1) #timedelta(days=day[10])
-    print(date)
-    
-    quit()
+    all_files = sorted(os.listdir(dirpath+str(itp)+'/itp'+str(itp)+'grddata/')) # list of files in the dir
+    ps, ts, ss, dates, lons, lats = np.array([]), np.array([]), np.array([]), np.array([]), np.array([]), np.array([]) # init lists
+    for n,file in enumerate(all_files): # loop through the files
+        filepath = dirpath+str(itp)+'/itp'+str(itp)+'grddata/'+file 
+        try: # try-except helps avoid breaking the code when opening a "bad" file
+            datapd = pd.read_csv(filepath, sep='\s+', skiprows=2, skipfooter=1, engine='python') # open the fleisch of the data
+            datapd = datapd.sort_values(by='day', ascending=True, ignore_index=True) # sort it so that it's time-increasing 
+            locpd = pd.read_csv(filepath, sep='\s+', nrows=2, engine='python') # top of CSV has the location
+            # format the date (subtract 1 from the day because Jan 1 is indexed from 1, so Jan 1 should correspond to timedelta(0 days))
+            date = [datetime(datapd['%year'][n],1,1,0,0,0,0) + timedelta(datapd['day'][n]-1) for n,i in enumerate(datapd['%year'])] 
+            p, t, s = datapd['pressure(dbar)'].values, datapd['temperature(C)'].values, datapd['salinity'].values # ctd data
+            lon, lat = np.full(len(t),locpd.iloc[0,2]), np.full(len(t),locpd.iloc[0,3]) # expand locations to full-length lists
+            ps, ts, ss = np.append(ps,p,axis=0), np.append(ts,t,axis=0), np.append(ss,s,axis=0) # save the data
+            dates, lons, lats = np.append(dates,date,axis=0), np.append(lons,lon,axis=0), np.append(lats,lat,axis=0)
+        except: 
+            print('Bad file: ' + filepath)
+            continue
+
+    # convert strings to floats
+    lons = lons.astype(float)
+    lats = lats.astype(float)
+
+    # now the data is saved in a series of very long 1D lists
+    # it isn't usable now, so let's put it into a Dataset and "regularize" it
     ds = xr.Dataset(
             data_vars=dict(
-                temperature=(['loc', 'di', 'date'], t),
-                pressure=(['loc', 'di', 'date'], p),
-                salinity=(['loc', 'di', 'date'], s),
+                temperature=(['date'], ts),
+                salinity=(['date'], ss),
+                pressure=(['date'], ps),
+                lon=(['date'], lons),
+                lat=(['date'], lats),
             ),
             coords=dict(
-                lon=('loc',lon),
-                lat=('loc',lat),
-                di=di,
-                date=date,
+                date=dates,
             ),
-            attrs=dict(description='Test desc.'),
+            attrs=dict(description='Test desc.'), # fill this out properly if saving the .nc
         )
-    print(ds)
+
+    # binning the pressures--likely unecessary but avoids any issues relating to abnormal pressure values
+    #pressure_levels = np.arange(1, 2001, 2) # down to 2000 meters (no way any ITP is deeper than this!)
+    #pressure_bins = [i for i in pd.cut(ds['pressure'], bins=pressure_levels, labels=pressure_levels[:-1]+1)]
+    #ds['pressure_bins'] = xr.DataArray(pressure_bins, dims='date') # add it as a variable
+    #ds = ds.drop_vars('pressure') # remove the old pressure var; it's now uneeded 
+
+    # resample the variables in time according to grouped pressures (thanks copilot!)
+    # check that this is correct if you're going to use this seriously
+    ds_grouped_resampled = ds.groupby('pressure').apply(lambda bin_group : bin_group.resample(date='1d',skipna=True).mean(skipna=True))
+
+    ds_grouped_resampled = ds_grouped_resampled.set_coords(['pressure']) # convert vars into coordinates
+    ds_2d = ds_grouped_resampled.set_index(z=('date','pressure')).drop_duplicates('z').unstack('z') 
+
+    return ds_2d
+
+def temp_hovm(ds,itp,dlim):
+    """Created a Hovmöller plot of temperature and pressure."""
+    plt.rcParams["font.family"] = "serif" # change the base font
+    f, ax = plt.subplots(figsize=(4, 3)) 
+    cmap = plt.colormaps['coolwarm']
+    cax = ds['temperature'].plot.contourf('date','pressure',ax=ax,levels=20,add_colorbar=False,cmap=cmap)
+    cbar = f.colorbar(cax, ax=ax)
+    cbar.ax.tick_params(labelsize=9)
+    cbar.ax.set_ylabel('Temperature ($\degree C$)')
+    ax.set_ylim(0,dlim)
+    ax.grid(True)
+    ax.set_ylabel('Pressure ($dbar$)',fontsize=11)
+    ax.set_xlabel('',fontsize=11)
+    ax.invert_yaxis()
+    ax.tick_params(axis='both',labelsize=9)
+    ax.set_title('Temperature for ITP '+str(itp),fontsize=12)
+    f.tight_layout()    
+    plt.savefig('Figures/ITP_'+str(itp)+'_temperature_hovm_4x3.png',bbox_inches='tight',dpi=450)
+    plt.savefig('Figures/ITP_'+str(itp)+'_temperature_hovm_4x3.pdf',format='pdf',bbox_inches='tight')
+
+
+def salt_hovm(ds,itp,dlim):
+    """Created a Hovmöller plot of salinity and pressure."""
+    plt.rcParams["font.family"] = "serif" # change the base font
+    f, ax = plt.subplots(figsize=(4, 3)) 
+    cmap = plt.colormaps['viridis']
+    cax = ds['salinity'].plot.contourf('date','pressure',ax=ax,levels=20,add_colorbar=False,cmap=cmap)
+    cbar = f.colorbar(cax, ax=ax)
+    cbar.ax.tick_params(labelsize=9)
+    cbar.ax.set_ylabel('Salinity ($PSU$)')
+    ax.set_ylim(0,dlim)
+    ax.grid(True)
+    ax.set_ylabel('Pressure ($dbar$)',fontsize=11)
+    ax.set_xlabel('',fontsize=11)
+    ax.invert_yaxis()
+    ax.tick_params(axis='both',labelsize=9)
+    ax.set_title('Salinity for ITP '+str(itp),fontsize=12)
+    f.tight_layout()    
+    plt.savefig('Figures/ITP_'+str(itp)+'_salinity_hovm_4x3.png',bbox_inches='tight',dpi=450)
+    plt.savefig('Figures/ITP_'+str(itp)+'_salinity_hovm_4x3.pdf',format='pdf',bbox_inches='tight')
+
+def temp_hovm_with_map(ds,itp,dlim):
+    """Created a Hovmöller plot of temperature and pressure with a map."""
+    plt.rcParams["font.family"] = "serif" # change the base font
+    f = plt.figure(figsize=(4,4.5)) #2,1,figsize=(4, 6),subplot_kw=dict(projection=projection)) 
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+    
+    southLat, northLat, westLon, eastLon = -85, -55, 179, -179
+    land_50m = feature.NaturalEarthFeature('physical', 'land', '50m',edgecolor='black', facecolor='black')
+    projection = ccrs.PlateCarree(central_longitude=(westLon+eastLon)/2)#ccrs.AlbersEqualArea(central_longitude=(westLon+eastLon)/2, central_latitude=(southLat+northLat)/2,standard_parallels=(southLat,northLat))
+    subplot_kw=dict(projection=projection,aspect=4)
+    ax1 = f.add_subplot(gs[0], **subplot_kw)
+    ax1.set_extent([westLon, eastLon, southLat, northLat], crs=ccrs.PlateCarree())
+    ax1.add_feature(land_50m, color=[0.8, 0.8, 0.8])
+    ax1.coastlines(resolution='50m')
+    ax1.scatter(ds['lon'],ds['lat'],marker='.',s=5,facecolors='red', edgecolors='red')
+    gl = ax1.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, linewidth=0.5)
+    gl.top_labels=False #suppress top labels
+    gl.right_labels=False #suppress right labels
+    gl.rotate_labels=False
+    gl.ylocator = mticker.FixedLocator([-85, -80, -75, -70, -65, -60, -55])  #[50, 55, 60, 65, 70, 75, 80])
+    gl.xlocator = mticker.FixedLocator([-180,-150,-120,-90,-60,-30,0,30,60,90,120,150,180])
+    gl.xlabel_style = {'size': 9}
+    gl.ylabel_style = {'size': 9}
+    ax1.set_title("Location",fontsize=11)
+
+    ax2 = f.add_subplot(gs[1])
+    cmap = plt.colormaps['coolwarm']
+    cax = ds['temperature'].plot.contourf('date','pressure',ax=ax2,levels=20,add_colorbar=False,cmap=cmap)
+    cbar = f.colorbar(cax, ax=ax2)
+    cbar.ax.tick_params(labelsize=9)
+    cbar.ax.set_ylabel('Temperature ($\degree C$)')
+    ax2.set_ylim(0,dlim)
+    ax2.grid(True)
+    ax2.set_ylabel('Pressure ($dbar$)',fontsize=11)
+    ax2.set_xlabel('',fontsize=11)
+    ax2.invert_yaxis()
+    ax2.tick_params(axis='both',labelsize=9)
+    ax2.set_title("Temperature",fontsize=11)
+
+    f.suptitle('ITP '+str(itp),fontsize=12)
+    f.tight_layout()    
+    plt.savefig('Figures/ITP_'+str(itp)+'_temperature_hovm_4x3.png',bbox_inches='tight',dpi=450)
+    plt.savefig('Figures/ITP_'+str(itp)+'_temperature_hovm_4x3.pdf',format='pdf',bbox_inches='tight')
+
+def salt_hovm_with_map(ds,itp,dlim):
+    """Created a Hovmöller plot of salinity and pressure with a map."""
+    plt.rcParams["font.family"] = "serif" # change the base font
+    f = plt.figure(figsize=(4,4.5)) #2,1,figsize=(4, 6),subplot_kw=dict(projection=projection)) 
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+    
+    southLat, northLat, westLon, eastLon = -85, -55, 179, -179
+    land_50m = feature.NaturalEarthFeature('physical', 'land', '50m',edgecolor='black', facecolor='black')
+    projection = ccrs.PlateCarree(central_longitude=(westLon+eastLon)/2)#ccrs.AlbersEqualArea(central_longitude=(westLon+eastLon)/2, central_latitude=(southLat+northLat)/2,standard_parallels=(southLat,northLat))
+    subplot_kw=dict(projection=projection,aspect=4)
+    ax1 = f.add_subplot(gs[0], **subplot_kw)
+    ax1.set_extent([westLon, eastLon, southLat, northLat], crs=ccrs.PlateCarree())
+    ax1.add_feature(land_50m, color=[0.8, 0.8, 0.8])
+    ax1.coastlines(resolution='50m')
+    ax1.scatter(ds['lon'],ds['lat'],marker='.',s=5,facecolors='red', edgecolors='red')
+    gl = ax1.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, linewidth=0.5)
+    gl.top_labels=False #suppress top labels
+    gl.right_labels=False #suppress right labels
+    gl.rotate_labels=False
+    gl.ylocator = mticker.FixedLocator([-85, -80, -75, -70, -65, -60, -55])  #[50, 55, 60, 65, 70, 75, 80])
+    gl.xlocator = mticker.FixedLocator([-180,-150,-120,-90,-60,-30,0,30,60,90,120,150,180])
+    gl.xlabel_style = {'size': 9}
+    gl.ylabel_style = {'size': 9}
+    ax1.set_title("Location",fontsize=11)
+
+    ax2 = f.add_subplot(gs[1])
+    cmap = plt.colormaps['viridis']
+    cax = ds['salinity'].plot.contourf('date','pressure',ax=ax2,levels=20,add_colorbar=False,cmap=cmap)
+    cbar = f.colorbar(cax, ax=ax2)
+    cbar.ax.tick_params(labelsize=9)
+    cbar.ax.set_ylabel('Salinity ($PSU$)')
+    ax2.set_ylim(0,dlim)
+    ax2.grid(True)
+    ax2.set_ylabel('Pressure ($dbar$)',fontsize=11)
+    ax2.set_xlabel('',fontsize=11)
+    ax2.invert_yaxis()
+    ax2.tick_params(axis='both',labelsize=9)
+    ax2.set_title("Salinity",fontsize=11)
+
+    f.suptitle('ITP '+str(itp),fontsize=12)
+    f.tight_layout()    
+    plt.savefig('Figures/ITP_'+str(itp)+'_salinity_hovm_4x3.png',bbox_inches='tight',dpi=450)
+    plt.savefig('Figures/ITP_'+str(itp)+'_salinity_hovm_4x3.pdf',format='pdf',bbox_inches='tight')
 
 if __name__=="__main__":
+    itp = 31
+    dlim = 700
+    ds = open_dat_file(itp)
+    #temp_hovm(ds,itp,dlim)    
+    #salt_hovm(ds,itp,dlim)  
+    temp_hovm_with_map(ds,itp,dlim)
+    salt_hovm_with_map(ds,itp,dlim)
+
+    itp = 40
+    dlim = 425
+    ds = open_dat_file(itp)
+    #temp_hovm(ds,itp,dlim)
+    #salt_hovm(ds,itp,dlim)
+    temp_hovm_with_map(ds,itp,dlim)
+    salt_hovm_with_map(ds,itp,dlim)
+
     itp = 132
-    open_dat_file(itp)
+    dlim = 250
+    ds = open_dat_file(itp)
+    #temp_hovm(ds,itp,dlim)
+    #salt_hovm(ds,itp,dlim)
+    temp_hovm_with_map(ds,itp,dlim)
+    salt_hovm_with_map(ds,itp,dlim)
--- marlin_analyses.py
+++ marlin_analyses.py
@@ -1,12 +1,19 @@
 # Rowan Brown, 13.01.2025
 # Munich, Germany
 
+import os
 import xarray as xr 
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import scipy.io as spio
 import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
+import matplotlib.path as mpath
+import cartopy.crs as ccrs
+import cartopy.feature as feature
+import matplotlib.ticker as mticker
+import matplotlib.colors as colors
+import numpy as np
 
 def open_marlin_data():
     """Opens Marlin float data in the mixed layer.
@@ -23,49 +30,6 @@ def open_marlin_data():
     ds = xr.open_dataset(fp)
     ds['time'] = [datetime(1, 1, 1, 0, 0) + timedelta(days=time - 367) for time in ds['time'].values] # 376 is necessary because you can only start datetime at 01-01-0001, plus an extra 2 days from who-knows-where (but I tested this with MATLAB datevec and 367 is correct)
     ds = ds.where((ds.temperature<2) & (ds.temperature>-2),np.nan)
-    return ds
-
-    # dealing with dates...
-    new_day_coord = list(range(85,462)) # we're going to interpolate onto these days, measure from the start of the year (I think)
-    start_date = datetime(2020,12,31,0,0,0) # I'm basically just assuming this is the start date
-    new_datetime_coords = [start_date + timedelta(days=i) for i in new_day_coord] # list need for creating the dataset
-
-    # locally-defined function for getting the time-weighted average of a variable over one day
-    def daily_avg_mooring_data(inmat): 
-        
-        # initializing a nan array, size is [number of days] x [number of depths]
-        out = np.empty((len(new_day_coord),6)) 
-        out[:] = np.nan
-        
-        # looping through the measured timesteps
-        for i in range(len(jul)):
-            if len(inmat[i]) > 0: # basically if we have a non-emtpy array
-                date = pd.to_datetime([start_date + timedelta(days=i[0]) for i in jul[i]]) # creating datetimes for each entry
-                df = pd.DataFrame(data={'date': date, 'var': [i[0] for i in inmat[i]]}) # filling a pandas dataframe
-                df['weights'] = [i.total_seconds() for i in df.diff().date] # seconds elapsed between measurements
-                df = df.set_index('date') 
-                df = df[(df.index > '2021-03-26') & (df.index < '2022-04-07')] # truncating start and end dates 
-                # taking the time-weighted average every 24 hours 
-                # weights don't align perfectly with each day, but it's close enough
-                new_df = df.resample('D').apply(lambda df : np.sum(df['weights']*df['var'])/np.sum(df['weights']))
-                out[:,i] = np.array(new_df) # writing to the "out" array
-
-        return out
-    
-    # creating an xarray dataset
-    ds = xr.Dataset(
-        data_vars=dict(
-            T=(["day","depth"], daily_avg_mooring_data(T)),
-            S=(["day","depth"], daily_avg_mooring_data(S)),
-            P=(["day","depth"], daily_avg_mooring_data(P)),
-        ),
-        coords=dict(
-            day=new_datetime_coords,
-            depth=[-50,-90,-135,-170,-220,-250],
-        ),
-        attrs=dict(description="Mooring data..."),
-    )
-
     return ds
 
 # plotting temperature
@@ -94,19 +58,45 @@ def temp_hovm(ds):
 
 # plotting temperature
 def temp_hovm_one_float(ds,id):
-    """Created a Hovmöller plot of temperature for just one float."""
+    """Created a Hovmöller plot of temperature for just one float with a map."""
+
     plt.rcParams["font.family"] = "serif" # change the base font
-    f, ax = plt.subplots(figsize=(4, 3))
-    cax = ds['temperature'].sel(buoy=id).plot.contourf('time','depth',ax=ax,levels=10,add_colorbar=False)
-    cbar = f.colorbar(cax, ax=ax)
+    f = plt.figure(figsize=(4,4.5)) #2,1,figsize=(4, 6),subplot_kw=dict(projection=projection)) 
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+    
+    southLat, northLat, westLon, eastLon = -85, -55, 179, -179
+    land_50m = feature.NaturalEarthFeature('physical', 'land', '50m',edgecolor='black', facecolor='black')
+    projection = ccrs.PlateCarree(central_longitude=(westLon+eastLon)/2)#ccrs.AlbersEqualArea(central_longitude=(westLon+eastLon)/2, central_latitude=(southLat+northLat)/2,standard_parallels=(southLat,northLat))
+    subplot_kw=dict(projection=projection,aspect=4)
+    ax1 = f.add_subplot(gs[0], **subplot_kw)
+    ax1.set_extent([westLon, eastLon, southLat, northLat], crs=ccrs.PlateCarree())
+    ax1.add_feature(land_50m, color=[0.8, 0.8, 0.8])
+    ax1.coastlines(resolution='50m')
+    ax1.scatter(ds['longitude'].sel(buoy=id).values,ds['latitude'].sel(buoy=id).values,marker='.',s=5,facecolors='red', edgecolors='red')
+    gl = ax1.gridlines(draw_labels=True, dms=True, x_inline=False, y_inline=False, linewidth=0.5)
+    gl.top_labels=False #suppress top labels
+    gl.right_labels=False #suppress right labels
+    gl.rotate_labels=False
+    gl.ylocator = mticker.FixedLocator([-85, -80, -75, -70, -65, -60, -55])  #[50, 55, 60, 65, 70, 75, 80])
+    gl.xlocator = mticker.FixedLocator([-180,-150,-120,-90,-60,-30,0,30,60,90,120,150,180])
+    gl.xlabel_style = {'size': 9}
+    gl.ylabel_style = {'size': 9}
+    ax1.set_title("Location",fontsize=11)
+
+    ax2 = f.add_subplot(gs[1])
+    cmap = plt.colormaps['coolwarm']
+    cax = ds['temperature'].sel(buoy=id).plot.contourf('time','depth',ax=ax2,levels=10,add_colorbar=False)
+    cbar = f.colorbar(cax, ax=ax2, cmap=cmap)
     cbar.ax.tick_params(labelsize=9)
     cbar.ax.set_ylabel('Temperature ($\degree C$)')
-    ax.grid(True) 
-    ax.set_ylabel('Depth ($m$)',fontsize=11)
-    ax.set_xlabel('',fontsize=9)
-    ax.set_title('Temperature from float '+str(id),fontsize=12)
-    ax.tick_params(axis='both',labelsize=9)
-    ax.invert_yaxis()
+    ax2.grid(True) 
+    ax2.set_ylabel('Depth ($m$)',fontsize=11)
+    ax2.set_xlabel('',fontsize=9)
+    ax2.set_title('Temperature',fontsize=11)
+    ax2.tick_params(axis='both',labelsize=9)
+    ax2.invert_yaxis()
+
+    f.suptitle('Float '+str(id),fontsize=12)    
     f.tight_layout()
     plt.savefig('Figures/Marlin_temperature_hovm_4x3_'+str(id)+'.png',bbox_inches='tight',dpi=450)
     plt.savefig('Figures/Marlin_temperature_hovm_4x3_'+str(id)+'.pdf',format='pdf',bbox_inches='tight')
@@ -114,4 +104,11 @@ def temp_hovm_one_float(ds,id):
 if __name__=="__main__":
     ds = open_marlin_data()
     temp_hovm(ds)
+    temp_hovm_one_float(ds,226781)
+    temp_hovm_one_float(ds,226782)
+    temp_hovm_one_float(ds,226783)
+    temp_hovm_one_float(ds,226784)
+    temp_hovm_one_float(ds,226785)
     temp_hovm_one_float(ds,226786)
+    temp_hovm_one_float(ds,227128)
+    temp_hovm_one_float(ds,227129)
