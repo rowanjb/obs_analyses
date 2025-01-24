3ec9721def33c1640a810d8910eec9bbcaef8ed3
rowanjb <rjb641@mun.ca>
2025-01-24 13:34:55 +0100

    Starting to look at density fluxes

--- mooring_analyses.py
+++ mooring_analyses.py
@@ -85,17 +85,110 @@ def open_mooring_ml_data():
     
     return ds
 
+def convective_resistance(ds):
+    """Calculates convective resistance, ie how much heat needs to be removed to cause homogenization. 
+    Reference depth is taken as 220 m, since this is the bottom working salinity sensor. 
+    Note: Convective resistence /assumes/ that none of the heat loss goes into creating sea ice; maybe see Wilson and M. for more on this. 
+        --->How to deal with pack ice? Some HF will make or melt ice, other will go into the water...
+    """
+    pot_rho, ref_depth = ds['pot_rho'], 220 # Variables needed for calculating the convective resistance 
+    pot_rho = pot_rho.where(pot_rho>0,drop=True) # Dropping whereever we had a temp but no salinity and tf no rho
+    pot_rho = pot_rho.assign_coords(dz=('depth',[(-1)*(pot_rho['depth'][n].values-d) for n,d in enumerate(np.append([0],pot_rho['depth'].values)[:-1])])) # Adding the 0 to get 50 at the start of the list  (ds['depth'][n]-ds['depth'][n-1])
+    term2 = pot_rho * pot_rho['dz']
+    convr = 9.81*(ref_depth * pot_rho.sel(depth=(-1)*ref_depth) - term2.sum(dim='depth'))
+    return convr
+
+def density_flux(ds):
+    """/Assumes through the surface/. Also neglects absolute surface (where dS is because) because lack of data.
+    Desnity flux represents the difference in mass in the water column between the stratified case and homogenized case."""
+
+    # We'll need dz later
+    # Adding the 0 to get 50 at the start of the list  (ds['depth'][n]-ds['depth'][n-1])
+    dz = lambda ds : [(-1)*(ds['depth'][n].values-d) for n,d in enumerate(np.append([0],ds['depth'].values)[:-1])] 
+    
+    # Density/mass anomaly
+    pot_rho, ref_depth = ds['pot_rho'], 220 # Variables needed for calculating the convective resistance 
+    pot_rho = pot_rho.where(pot_rho>0,drop=True) # Dropping whereever we had a temp but no salinity and tf no rho
+    pot_rho = pot_rho.assign_coords(dz=('depth', dz(pot_rho)))
+    term2 = pot_rho * pot_rho['dz']
+    dens_flux = ref_depth * pot_rho.sel(depth=(-1)*ref_depth) - term2.sum(dim='depth') 
+
+    # Heat content (anomaly?)
+    refT, rho_0, C_p = -1.8, 1026, 3992 # Alternative: ds['Cp'] = gsw.cp_t_exact(ds['S'],ds['T'],ds['p_from_z'])  
+    T = ds['T']
+    T = T.assign_coords(dz=('depth', dz(T)))
+    HC = rho_0 * C_p * 10**(-9) * ((T.sel(depth=slice(0,(-1)*ref_depth))-refT)*T['dz']).sum(dim='depth') # the 10^-9 makes the result GJ
+
+    # Salt content
+    S = ds['S'].where(ds['S']>0,drop=True)
+    S = S.assign_coords(dz=('depth',dz(S)))
+    SC = (S*S['dz']).sum(dim='depth')
+    
+    plt.rcParams["font.family"] = "serif" # change the base font
+    f, ax = plt.subplots(figsize=(7, 4))
+    color = 'tab:blue'
+    dens_flux.plot(ax=ax,color=color)
+    ax.set_ylabel('Mass anomaly ($kg$)',fontsize=11)
+    ax.set_xlabel('',fontsize=11)
+    ax.tick_params(size=9)
+    ax.set_ylim(0,20)
+    ax.set_title('Density changes at the Weddell Sea mooring',fontsize=12)
+    ax.tick_params(size=9,color=color)
+    ax.yaxis.label.set_color(color=color)
+    ax.tick_params(axis='y', colors=color)
+
+    ax2 = ax.twinx()
+    color = 'tab:red'
+    HC.plot(ax=ax2,color=color)
+    ax2.set_ylabel('Heat content ($GJ$)',fontsize=11,color=color)
+    ax2.set_xlabel('',fontsize=11)
+    ax2.tick_params(size=9,color=color)
+    ax2.yaxis.label.set_color(color=color)
+    ax2.tick_params(axis='y', colors=color)
+
+    ax3 = ax.twinx()
+    color = 'tab:green'
+    SC.plot(ax=ax3,color=color)
+    ax3.set_ylabel('Salt content ($PSU$)',fontsize=11,color=color)
+    ax3.set_xlabel('',fontsize=11)
+    ax3.tick_params(size=9,color=color)
+    ax3.yaxis.label.set_color(color=color)
+    ax3.tick_params(axis='y', colors=color)
+    ax3.spines.right.set_position(("axes", 1.2))
+
+    # Adding the sea ice data to the plot
+    with open('../filepaths/sea_ice_concentration') as f: dirpath = f.readlines()[0][:-1] # the [0] accesses the first line, and the [:-1] removes the newline tag
+    filepath = dirpath + '/sea_ice_concentration.nc'
+    id = select_nearest_coord(longitude = -27.0048333, latitude = -69.0005000) # Note 332.9125, -69.00584 is only 3360.27 m from the mooring
+    ds_si = xr.open_dataset(filepath).sel(date=slice("2021-03-26", "2022-04-06")).isel(x=id[0],y=id[1])
+    ax4 = ax.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:grey'
+    ax4.set_ylabel('Sea ice concentration ($\%$)', color=color, fontsize=11)  # we already handled the x-label with ax1
+    ax4.plot(ds_si['date'], ds_si['ice_conc'][:,0,0], color=color, linewidth=1)
+    ax4.tick_params(axis='y', labelcolor=color)
+    ax4.spines.right.set_position(("axes", 1.4))
+
+    plt.savefig('density_flux.png',bbox_inches='tight',dpi=250)
+
+    return dens_flux
+
+# EGU:
+# do the same thing for salinity and then say, based on the fluxes of salinity and heat that we have, regardless of the concentration 
+# of sea ice, we know what induced overturning. Then we can use ERA5/IFS plus satellite concentrations to guess where else this
+# could happen, and we can model the salt+heat flux using MITgcm (once we also have a good resolution, mixing scheme, and boundary
+# conditions)
+
 # plotting temperature
 def temp_hovm(ds):
     """Created a Hovmöller plot of temperature."""
     plt.rcParams["font.family"] = "serif" # change the base font
     f, ax = plt.subplots(figsize=(4, 4))
-    ds.T.sel(depth=[-50,-135,-220]).plot.contourf('day','depth',ax=ax,levels=20,cbar_kwargs={'label': 'Temperature ($\degree C$)', 'orientation': 'horizontal'})
+    ds.T.sel(depth=[-50, -90, -135, -170, -220]).plot.contourf('day','depth',ax=ax,levels=20,cbar_kwargs={'label': 'Temperature ($\degree C$)', 'orientation': 'horizontal'})
     ax.set_ylabel('Depth ($m$)',fontsize=11)
     ax.set_xlabel('',fontsize=11)
     ax.tick_params(size=9)
     ax.set_title('Temperature at the Weddell Sea mooring',fontsize=12)
-    
+
     # Adding the sea ice data to the plot
     with open('../filepaths/sea_ice_concentration') as f: dirpath = f.readlines()[0][:-1] # the [0] accesses the first line, and the [:-1] removes the newline tag
     filepath = dirpath + '/sea_ice_concentration.nc'
@@ -107,6 +200,15 @@ def temp_hovm(ds):
     ax2.plot(ds_si['date'], ds_si['ice_conc'][:,0,0], color=color, linewidth=1)
     ax2.tick_params(axis='y', labelcolor=color)
 
+    # Adding convective resistance to the plot
+    convr = convective_resistance(ds) # kind of recursive...
+    ax3 = ax.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:red'
+    ax3.spines.right.set_position(("axes", 1.25))
+    ax3.set_ylabel('Convective resistance ($J$ $m^{-3}$)', color=color, fontsize=11)  # we already handled the x-label with ax1
+    ax3.plot(convr['day'], convr, color=color, linewidth=1)
+    ax3.tick_params(axis='y', labelcolor=color)
+
     plt.savefig('Figures/Mooring_temperature_hovm_4x4.png',bbox_inches='tight',dpi=450)
     plt.savefig('Figures/Mooring_temperature_hovm_4x4.pdf',format='pdf',bbox_inches='tight')
 
@@ -115,7 +217,7 @@ def sal_hovm(ds):
     """Created a Hovmöller plot of salinity."""
     plt.rcParams["font.family"] = "serif" # change the base font
     f, ax = plt.subplots(figsize=(4, 4))
-    p = ds.S.sel(depth=[-50, -90, -135, -170, -220]).plot.contourf('day','depth',ax=ax,levels=10,add_colorbar=False)
+    p = ds.S.sel(depth=[-50, -135, -220]).plot.contourf('day','depth',ax=ax,levels=10,add_colorbar=False)
     ax.set_ylabel('Depth ($m$)',fontsize=11)
     ax.set_xlabel('',fontsize=11)
     ax.tick_params(size=9)
@@ -133,6 +235,15 @@ def sal_hovm(ds):
     ax2.set_ylabel('Sea ice concentration ($\%$)', color=color, fontsize=11)  # we already handled the x-label with ax1
     ax2.plot(ds_si['date'], ds_si['ice_conc'][:,0,0], color=color, linewidth=1)
     ax2.tick_params(axis='y', labelcolor=color)
+
+    # Adding convective resistance to the plot
+    convr = convective_resistance(ds) # kind of recursive...
+    ax3 = ax.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:red'
+    ax3.spines.right.set_position(("axes", 1.25))
+    ax3.set_ylabel('Convective resistance ($J$ $m^{-3}$)', color=color, fontsize=11)  # we already handled the x-label with ax1
+    ax3.plot(convr['day'], convr, color=color, linewidth=1)
+    ax3.tick_params(axis='y', labelcolor=color)
 
     plt.savefig('Figures/Mooring_salinity_hovm_4x4.png',bbox_inches='tight',dpi=450)
     plt.savefig('Figures/Mooring_salinity_hovm_4x4.pdf',format='pdf',bbox_inches='tight')
@@ -161,11 +272,21 @@ def rho_hovm(ds):
     ax2.plot(ds_si['date'], ds_si['ice_conc'][:,0,0], color=color, linewidth=1)
     ax2.tick_params(axis='y', labelcolor=color)
 
+    # Adding convective resistance to the plot
+    convr = convective_resistance(ds) # kind of recursive...
+    ax3 = ax.twinx()  # instantiate a second Axes that shares the same x-axis
+    color = 'tab:red'
+    ax3.spines.right.set_position(("axes", 1.25))
+    ax3.set_ylabel('Convective resistance ($J$ $m^{-3}$)', color=color, fontsize=11)  # we already handled the x-label with ax1
+    ax3.plot(convr['day'], convr, color=color, linewidth=1)
+    ax3.tick_params(axis='y', labelcolor=color)
+
     plt.savefig('Figures/Mooring_density_hovm_4x4.png',bbox_inches='tight',dpi=450)
     plt.savefig('Figures/Mooring_density_hovm_4x4.pdf',format='pdf',bbox_inches='tight')
 
 if __name__=="__main__":
     ds = open_mooring_ml_data()
-    temp_hovm(ds)
-    sal_hovm(ds)
-    rho_hovm(ds)
+    density_flux(ds)
+    #temp_hovm(ds)
+    #sal_hovm(ds)
+    #rho_hovm(ds)
